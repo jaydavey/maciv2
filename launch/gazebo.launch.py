3a7ae15a321f02962c78c78b0ef3c92bf7c1927f
jaydavey <jay.davey87@gmail.com>
2024-07-31 22:42:15 +0000

    updating launch file, removing problem file

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -6,15 +6,14 @@ from launch_param_builder import load_xacro
 from os.path import join
 from pathlib import Path
 
-def generate_launch_description():
 
-    path = join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py")  
-    coke_world = join(get_package_share_directory("maciv2"), "models","cokeworld2.sdf")
-  
-    gazebo_sim = IncludeLaunchDescription(path, launch_arguments=[("gz_args",  coke_world)])
+
+def generate_launch_description():
+    path = join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py")    
+    gazebo_sim = IncludeLaunchDescription(path, launch_arguments=[("gz_args",  "empty.sdf")])
 
     robot = ExecuteProcess(
-        cmd=["ros2", "run", "ros_gz_sim", "create", "-topic", "robot_description", '-z', '1.0'],
+        cmd=["ros2", "run", "ros_gz_sim", "create", "-topic", "robot_description", "-x", "-6.70", "-y","-9.34", "-z", "1.23"],
         name="spawn robot",
         output="both"
     )
@@ -30,27 +29,4 @@ def generate_launch_description():
                      'use_sim_time':True}],
     )
 
-    start_controllers  = Node(
-                package="controller_manager",
-                executable="spawner",
-                arguments=['joint_state_broadcaster', 'arm_controller', 'gripper_controller'],
-                output="screen",
-            )
-    
-    move_group = IncludeLaunchDescription(join(get_package_share_directory("maciv2_moveit"), "launch", "move_group.launch.py"))
-    rviz = IncludeLaunchDescription(join(get_package_share_directory("maciv2_moveit"), "launch", "moveit_rviz.launch.py"))
-
-    # Gazebo Bridge: This brings data (sensors/clock) out of gazebo into ROS.
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
-                   '/realsense/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
-                   ],
-        output='screen'
-        )
-    
-    mg_sim_time = ExecuteProcess(cmd=["ros2", "param", "set", "/move_group", "use_sim_time","True"])
-    rviz_sim_time = ExecuteProcess(cmd=["ros2", "param", "set", "/rviz", "use_sim_time","True"])
-
-    return LaunchDescription([gazebo_sim, robot, robot_state_publisher, start_controllers, move_group, rviz, bridge, mg_sim_time, rviz_sim_time ])
+    return LaunchDescription([gazebo_sim, robot, robot_state_publisher])
